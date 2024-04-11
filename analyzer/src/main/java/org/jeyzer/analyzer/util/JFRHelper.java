e6be7079c883f1a9e91341caf5874f4079f22e39
Jeyzer Admin <admin@jeyzer.org>
2024-04-11 07:58:37 +0200

    JEYZ-99 : Reject the multiple JFR files analysis (provided in zip file) - header added

--- analyzer/src/main/java/org/jeyzer/analyzer/util/JFRHelper.java
+++ analyzer/src/main/java/org/jeyzer/analyzer/util/JFRHelper.java
@@ -1,5 +1,17 @@
 package org.jeyzer.analyzer.util;
 
+/*-
+ * ---------------------------LICENSE_START---------------------------
+ * Jeyzer Analyzer
+ * --
+ * Copyright (C) 2020 - 2024 Jeyzer
+ * --
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * ----------------------------LICENSE_END----------------------------
+ */
+
 import java.io.File;
 
 public class JFRHelper {
